b08d53ffc8a207c186fdb48e7008d72c283be71c
Shevewinyei <46924288+Shevewinyei@users.noreply.github.com>
2020-07-06 01:05:35 +0800

    Update BeanAdmin.java

--- oline_fresh_supermaket/src/oline_fresh_supermaket/model/BeanAdmin.java
+++ oline_fresh_supermaket/src/oline_fresh_supermaket/model/BeanAdmin.java
@@ -1,7 +1,31 @@
 package oline_fresh_supermaket.model;
 
-public class BeanAdmin {
 
+public class BeanAdmin {
+	public static BeanAdmin currentLoginUser=null;
+	int admin_id;
+	String admin_name;
+	String admin_pwd;
+	
+	public int getAdmin_id() {
+		return admin_id;
+	}
+	public void setAdmin_id(int admin_id) {
+		this.admin_id = admin_id;
+	}
+	public String getAdmin_name() {
+		return admin_name;
+	}
+	public void setAdmin_name(String admin_name) {
+		this.admin_name = admin_name;
+	}
+	public String getAdmin_pwd() {
+		return admin_pwd;
+	}
+	public void setAdmin_pwd(String admin_pwd) {
+		this.admin_pwd = admin_pwd;
+	}
+	
 	
 	
 	
