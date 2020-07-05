f3cfdd9353dbf095d2ddadcacad215aa57744339
Shevewinyei <46924288+Shevewinyei@users.noreply.github.com>
2020-07-06 01:05:31 +0800

    Create Beanaddress.java

---
+++ oline_fresh_supermaket/src/oline_fresh_supermaket/model/Beanaddress.java
@@ -0,0 +1,46 @@
+package oline_fresh_supermaket.model;
+
+public class Beanaddress {
+	int addr_id;
+	int usr_id;
+	String addr_pro;
+	String addr_city;
+	String addr_area;
+	String addr_current;
+	public int getAddr_id() {
+		return addr_id;
+	}
+	public void setAddr_id(int addr_id) {
+		this.addr_id = addr_id;
+	}
+	public int getUsr_id() {
+		return usr_id;
+	}
+	public void setUsr_id(int usr_id) {
+		this.usr_id = usr_id;
+	}
+	public String getAddr_pro() {
+		return addr_pro;
+	}
+	public void setAddr_pro(String addr_pro) {
+		this.addr_pro = addr_pro;
+	}
+	public String getAddr_city() {
+		return addr_city;
+	}
+	public void setAddr_city(String addr_city) {
+		this.addr_city = addr_city;
+	}
+	public String getAddr_area() {
+		return addr_area;
+	}
+	public void setAddr_area(String addr_area) {
+		this.addr_area = addr_area;
+	}
+	public String getAddr_current() {
+		return addr_current;
+	}
+	public void setAddr_current(String addr_current) {
+		this.addr_current = addr_current;
+	}
+}
