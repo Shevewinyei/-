0ab1be4651412f1465e4d56123ef74a01c91201a
Shevewinyei <46924288+Shevewinyei@users.noreply.github.com>
2020-07-10 14:39:12 +0800

    Update FrmBuyCommodity.java

--- oline_fresh_supermaket/src/oline_fresh_supermaket/ui/FrmBuyCommodity.java
+++ oline_fresh_supermaket/src/oline_fresh_supermaket/ui/FrmBuyCommodity.java
@@ -6,10 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JComboBox;
+import javax.swing.JTable;
 
 public class FrmBuyCommodity extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -32,11 +37,23 @@ public class FrmBuyCommodity extends JFrame {
 	 */
 	public FrmBuyCommodity() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 570, 594);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		table = new JTable();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(table, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(480, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addComponent(table, GroupLayout.DEFAULT_SIZE, 562, Short.MAX_VALUE)
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
-
 }
